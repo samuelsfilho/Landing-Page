7826557609846079031e13279a5860a5c45e4fd2
Samuel Silva <samuelcassimirof@gmail.com>
2025-01-21 13:43:12 +0000

    unica

--- Projeto Personal/src/index.js
+++
@@ -1,8 +0,0 @@
-
-// // Formulário de contato
-const form = document.getElementById('contactForm');
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    alert('Formulário enviado! Entraremos em contato em breve.');
-    form.reset();
-});
